fee5efa55faaf754e7e8f3aca1ce7440d1d67ca3
Robertcego <lrobertito@gmail.com>
2021-08-03 13:49:10 -0600

    update

--- src/utils/randomHexGenerator.js
+++ src/utils/randomHexGenerator.js
@@ -1,9 +1,10 @@
+/** @format */
 
-const random_hex_color_code = () => {
-    let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    return '#' + n.slice(0, 6);
-  };
-  
-  console.log(random_hex_color_code());
+const randomHexGenerator = () => {
+  let n = (Math.random() * 0xfffff * 1000000).toString(16);
+  return '#' + n.slice(0, 6);
+};
 
-  export default random_hex_color_code;
+console.log(randomHexGenerator());
+
+export default randomHexGenerator;
